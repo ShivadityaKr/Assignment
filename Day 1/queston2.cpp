1a5b7a88a97c63a7bb83133a52290f11506ae46a
Shivaditya Kr <71021935+ShivadityaKr@users.noreply.github.com>
2022-01-18 15:48:02 +0530

    comment

--- Day 1/queston2.cpp
+++ Day 1/queston2.cpp
@@ -1,3 +1,4 @@
+// ii) Write a program (without using inbuilt functions and not using Strings or array) to swap first and last digits of any number
 #include<iostream>
 using namespace std;
 int main(){
